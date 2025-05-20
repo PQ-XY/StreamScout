e950058be05a662518b27f8a86bb8ea153927608
Yao Xu <yaoxu369@gmail.com>
2025-05-19 21:29:04 -0400

    test file update

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const originalError = console.error;
+
+console.error = (...args) => {
+  if (
+    args[0].includes('[antd: Modal] `visible` is deprecated') ||
+    args[0].includes('[antd: Modal] `destroyOnClose` is deprecated')
+  ) {
+    return;
+  }
+  originalError(...args);
+};
